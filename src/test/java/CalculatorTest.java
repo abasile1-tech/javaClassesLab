00b246527503913305cff389fb445f97d32f3cd9
John <ashton.basile@gmail.com>
2023-09-18 16:12:41 +0100

    I wrote tests for the Calculator class as part of TDD

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,28 @@
+import com.codebase.Calculator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    @Test
+    public void canAdd() {
+        assertEquals(4, Calculator.add(2,2));
+    }
+
+    @Test
+    public void canSubtract() {
+        assertEquals(2, Calculator.subtract(4,2));
+    }
+
+    @Test
+    public void canMultiply() {
+        assertEquals(8, Calculator.multiply(4,2));
+    }
+
+    @Test
+    public void canDivide() {
+        assertEquals(4, Calculator.divide(8,2));
+    }
+
+
+}
