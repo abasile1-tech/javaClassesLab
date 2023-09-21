ab1d2cb9c29874650c9abba2a3144d62c68ce6fb
John <ashton.basile@gmail.com>
2023-09-21 10:20:11 +0100

    I used the java version of namespace which is packages to be able to just call the individual methods in the correct namespace/package

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,27 +1,31 @@
 import com.codebase.Calculator;
 import org.junit.Test;
+import static com.codebase.Calculator.add;
+import static com.codebase.Calculator.subtract;
+import static com.codebase.Calculator.multiply;
+import static com.codebase.Calculator.divide;
 
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
     @Test
     public void canAdd() {
-        assertEquals(4, Calculator.add(2,2));
+        assertEquals(4, add(2,2));
     }
 
     @Test
     public void canSubtract() {
-        assertEquals(2, Calculator.subtract(4,2));
+        assertEquals(2, subtract(4,2));
     }
 
     @Test
     public void canMultiply() {
-        assertEquals(8, Calculator.multiply(4,2));
+        assertEquals(8, multiply(4,2));
     }
 
     @Test
     public void canDivide() {
-        assertEquals(4, Calculator.divide(8,2), 0.0);
+        assertEquals(4, divide(8,2), 0.0);
     }
 
 
